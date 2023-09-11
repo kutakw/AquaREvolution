cd723ccde50f423ba41c52583a435df666f593ec
Lorak294 <karol.kacki2@gmail.com>
2023-09-11 09:44:40 +0200

    made applying mutations randomized

--- AquaEvolution/config.h
+++ AquaEvolution/config.h
@@ -5,40 +5,73 @@
 #include <thrust/device_vector.h>
 
 // Environment dimensions
-static constexpr float WIDTH = 100.f;
-static constexpr float HEIGHT = 100.f;
+constexpr float WIDTH = 100.f;
+constexpr float HEIGHT = 100.f;
 
 // Number of objects present during initialization
-static constexpr uint64_t FISH_START = 100;
-static constexpr uint64_t ALGAE_START = 1000;
+constexpr uint64_t FISH_START = 100;
+constexpr uint64_t ALGAE_START = 1000;
 
 // Cell dimensions
-static constexpr ulonglong2 CELL = { 100, 100 };
+constexpr ulonglong2 CELL = { 100, 100 };
 
 // Duration single generation simulation
-static constexpr int32_t ITER_PER_GENERATION = 1000;
+constexpr int32_t ITER_PER_GENERATION = 1000;
 
 // Alage parameters
-static constexpr uint64_t ALGAE_MAX_COUNT = 30000;
-static constexpr float ALGAE_INIT_ENERGY = 25.0f;
-static constexpr float ALGAE_MAX_ENERGY = 50.0f;
-static constexpr float ALGAE_ENERGY_LOSS = 0.1f;
-static constexpr float ALGAE_ENERGY_MINIMUM_TO_REPRODUCT = 10.0f;
-static constexpr float ALGAE_ENERGY_PER_KID = 10.0f;
-static constexpr float ALGAE_VELOCITY = 1e-3f;
+constexpr uint64_t ALGAE_MAX_COUNT = 30000;
+constexpr float ALGAE_INIT_ENERGY = 25.0f;
+constexpr float ALGAE_MAX_ENERGY = 50.0f;
+constexpr float ALGAE_ENERGY_LOSS = 0.1f;
+constexpr float ALGAE_ENERGY_MINIMUM_TO_REPRODUCT = 10.0f;
+constexpr float ALGAE_ENERGY_PER_KID = 10.0f;
+constexpr float ALGAE_VELOCITY = 1e-3f;
 
 // Fish parameters
-static constexpr uint64_t FISH_MAX_COUNT = 10000;
-static constexpr float FISH_MAX_ENERGY = 50.0f;
-static constexpr float FISH_INITAL_ENERGY = 30.0f;
-static constexpr float FISH_ENERGY_PER_KID = 10.0f;
-static constexpr float FISH_ENERGY_MINIMUM_TO_REPRODUCT = 5.0f;
-static constexpr float FISH_ENERGY_PER_ALGA_EATEN = 3.0f;
-static constexpr float FISH_SIGHT_DIST = 10.0f;
-static constexpr float FISH_SIGHT_ANGLE = 0.0f;
-static constexpr float FISH_VELOCITY = 2e-3f;
-static constexpr float FISH_ENERGY_DECAY_RATE = 0.1f;
+constexpr uint64_t FISH_MAX_COUNT = 10000;
+constexpr float FISH_MAX_ENERGY = 50.0f;
+constexpr float FISH_INITAL_ENERGY = 30.0f;
+constexpr float FISH_ENERGY_PER_KID = 10.0f;
+constexpr float FISH_ENERGY_MINIMUM_TO_REPRODUCT = 5.0f;
+constexpr float FISH_ENERGY_PER_ALGA_EATEN = 3.0f;
+constexpr float FISH_SIGHT_DIST = 10.0f;
+constexpr float FISH_SIGHT_ANGLE = 0.0f;
+constexpr float FISH_VELOCITY = 2e-3f;
+constexpr float FISH_ENERGY_DECAY_RATE = 0.1f;
 
+// Mutations
+constexpr uint64_t MUTATION_COUNT = 6;
+inline void initMutationsFromConfig(Mutation& mutation) {
+	
+	mutation.resize(mutation.host, MUTATION_COUNT);
+
+	// M0 -> faster but less energy-capacity
+	mutation.host.energyAlteration[0] = make_float2(0.995f, 1.0f);
+	mutation.host.sightAlteration[0] = make_float2(1.0f, 0.f); // (dist -> multiplier, angle -> addition)
+	mutation.host.velocityAlteration[0] = 1.005f;
+	// M1 -> more enrgy capacity but slower
+	mutation.host.energyAlteration[1] = make_float2(1.005f, 1.0f);
+	mutation.host.sightAlteration[1] = make_float2(1.0f, 0.0f); // (dist -> multiplier, angle -> addition)
+	mutation.host.velocityAlteration[1] = 0.995f;
+	// M2 -> faster but more energy usage
+	mutation.host.energyAlteration[2] = make_float2(1.0f, 1.005f);
+	mutation.host.sightAlteration[2] = make_float2(1.0f, 0.0f); // (dist -> multiplier, angle -> addition)
+	mutation.host.velocityAlteration[2] = 1.005f;
+	// M3 -> slower but less energy usage
+	mutation.host.energyAlteration[3] = make_float2(1.0f, 0.995f);
+	mutation.host.sightAlteration[3] = make_float2(1.0f, 0.0f); // (dist -> multiplier, angle -> addition)
+	mutation.host.velocityAlteration[3] = 0.995f;
+	// M4 -> further sight distance but less angle
+	mutation.host.energyAlteration[4] = make_float2(1.0f, 1.0f);
+	mutation.host.sightAlteration[4] = make_float2(1.005f, -0.001f); // (dist -> multiplier, angle -> addition)
+	mutation.host.velocityAlteration[4] = 1.0f;
+	// M5 -> closer sight distance but bigger anlge
+	mutation.host.energyAlteration[5] = make_float2(1.0f, 1.0f);
+	mutation.host.sightAlteration[5] = make_float2(0.995f, 0.001f); // (dist -> multiplier, angle -> addition)
+	mutation.host.velocityAlteration[5] = 1.0f;
+}
+
+// Data file dumping frequency (one dump per DUMP_FREQ number of generations)
 static constexpr int32_t DUMP_FREQ = 10;
 
 #endif // !CONFIG_H
--- AquaEvolution/kernel.cu
+++ AquaEvolution/kernel.cu
@@ -1,7 +1,9 @@
 ﻿#include <window.cuh>
+#include <time.h>
 
 int main(int argc, char* argv[])
 {
+	srand(time(NULL));
 	Window& instance = Window::instance();
 	Aquarium aquarium;
 	instance.renderLoop(aquarium);
